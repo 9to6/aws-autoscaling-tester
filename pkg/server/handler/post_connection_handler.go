bc2667f18d5febb965a9b87b7eb639953050f1ab
tkwon <tkwon@gracenote.com>
2018-08-13 15:16:55 +0900

    updated post connection handler

--- pkg/server/handler/post_connection_handler.go
+++ pkg/server/handler/post_connection_handler.go
@@ -12,6 +12,10 @@ func PostConnectionHandler(c *gin.Context) {
 	l_, ok := c.Get("logger")
 	if ok == false {
 		fmt.Println("error--")
+		c.AbortWithStatusJSON(500, gin.H{
+			"message": "logger error",
+		})
+		c.Header("Content-Type", "application/json")
 		return
 	}
 	l := l_.(*llog.Logger)
@@ -20,6 +24,11 @@ func PostConnectionHandler(c *gin.Context) {
 	connection, err := strconv.ParseInt(connStr, 10, 32)
 	if err != nil {
 		l.Error(err)
+		c.JSON(500, gin.H{
+			"message": "you have to input 'conn' params",
+		})
+		c.AbortWithError(500, err)
+		c.Header("Content-Type", "application/json")
 		return
 	}
 	l.Info("connection parameter is ", connection)
